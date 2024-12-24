bf618ce8fd5f6c17cf48dfc8b1a3778b35f3e30d
YASMEEN <yasmeennaaz@gmail.com>
2024-12-24 15:22:31 +0530

    first commit

---
+++ Scripts/main.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading =  React.createElement('h1',{},"Hello World from React");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
